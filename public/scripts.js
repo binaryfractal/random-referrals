1af74d1ba5ecdac13d060a3881a2636f9efbb089
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-05-30 23:26:16 -0500

    Validations

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,10 @@
-url = "https://{URL_API}"
+url = "https://{URL_API}";
+
+configStorage = {
+    platform: 'XXXXXX', // Save last date where registred code
+    lastDayGenerated: 'XXXXXX', // Save last date where the code was generated
+    totalGenerated: 'XXXXXX' // Total code generated
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
@@ -8,16 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
 async function getRandomCode() {
     document.querySelector('.progress-random-code').style.display = 'block';
 
-    const platform = document.querySelector('select').value;
-    const response = await fetch(
-        `${url}/${platform}`, {
-            mode: 'cors',
-            method: 'GET'
-        }
-    );
+    if(await validateGenerator()) {
+        const platform = document.querySelector('select').value;
+        const response = await fetch(
+            `${url}/${platform}`, {
+                mode: 'cors',
+                method: 'GET'
+            }
+        );
 
-    const result = await response.text();
-    document.querySelector('#random_code').value = result;
+        const result = await response.text();
+        document.querySelector('#random_code').value = result;
+        await registerGenerator();
+    } else {
+        M.toast({html: 'Haz generado el número maximo de códigos diarios', classes: 'red'});
+    }
+
     document.querySelector('.progress-random-code').style.display = 'none';
 }
 
@@ -31,20 +43,92 @@ async function registerCode() {
 
     const platform = document.querySelector('select').value;
     document.querySelector('.progress-register-code').style.display = 'block';
-    const response = await fetch(
-        `${url}/${platform}/${code}`, {
-            mode: 'cors',
-            method: 'POST'
-        }
-    );
 
-    const result = await response.text();
-    if(result.toString() === false) {
-        M.toast({html: 'Error al guardar el código', classes: 'red'});
+    if(await validateRegistrator(platform)) {
+        const response = await fetch(
+            `${url}/${platform}/${code}`, {
+                mode: 'cors',
+                method: 'POST'
+            }
+        );
+    
+        const result = await response.text();
+        if(result.toString() === false) {
+            M.toast({html: 'Error al guardar el código', classes: 'red'});
+        } else {
+            M.toast({html: 'Código guardado con existo', classes: 'green'});
+            document.querySelector('#register_code').value = '';
+
+            await registerRegistrator(platform);
+        }
     } else {
-        M.toast({html: 'Código guardado con existo', classes: 'green'});
-        document.querySelector('#register_code').value = '';
+        M.toast({html: 'Ya registraste código para esta plataforma', classes: 'red'});
     }
 
     document.querySelector('.progress-register-code').style.display = 'none';
+}
+
+async function registerGenerator() {
+    let total = 0;
+    if( configStorage.lastDayGenerated in localStorage &&
+        configStorage.totalGenerated in localStorage) {
+            
+        const totalGeneratedStorage = localStorage.getItem(configStorage.totalGenerated);
+        total = 0;
+        if(totalGeneratedStorage) {
+            total = parseInt(totalGeneratedStorage) + 1;
+        }
+    }
+
+    localStorage.removeItem(configStorage.totalGenerated);
+    localStorage.removeItem(configStorage.lastDayGenerated);
+
+    localStorage.setItem(configStorage.totalGenerated, total);
+    localStorage.setItem(configStorage.lastDayGenerated,  new Date().getDay());
+}
+
+async function validateGenerator() {
+    if( configStorage.lastDayGenerated in localStorage &&
+        configStorage.totalGenerated in localStorage) {
+            
+        const lastDayStorage = localStorage.getItem(configStorage.lastDayGenerated);
+        const totalGeneratedStorage = localStorage.getItem(configStorage.totalGenerated);
+
+        let total = 0;
+        if(totalGeneratedStorage) {
+            total = parseInt(totalGeneratedStorage);
+        } 
+
+        if(total < 2) {
+            return true;
+        } else {
+            if(lastDayStorage !== new Date().getDay().toString()) {
+                await resetGenerator();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+async function resetGenerator() {
+    localStorage.setItem(configStorage.totalGenerated, 0);
+}
+
+async function registerRegistrator(platform) {
+    localStorage.removeItem(`${configStorage.platform}_${platform}`);
+    localStorage.setItem(`${configStorage.platform}_${platform}`, platform);
+}
+
+async function validateRegistrator(platform) {
+    const platformStorage = localStorage.getItem(`${configStorage.platform}_${platform}`);
+
+    if(!platformStorage) {
+        return true;
+    }
+
+    return false;
 }
