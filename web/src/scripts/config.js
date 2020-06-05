9b11f3cae1683e32b8594635e36119cafcbab9bf
Cesar Alberto Nava Chavez <ing.cesarchaveznava@gmail.com>
2020-06-05 17:20:49 -0500

    Ranking and New Platforms

--- web/src/scripts/config.js
+++ web/src/scripts/config.js
@@ -1,6 +1,6 @@
 export class Config {
-    static PLATFORM_KEY = '9ISTZ9y2RaVNRUmDkn0D'; 
-    static LAST_DAY_GENERATED_KEY = 'iIOoYdh9Lo8qOmnQlnZ7'; 
-    static TOTAL_GENERATED_KEY = 'H0hA7FQdRtsvDkDQ6Svy'; 
-    static URL = 'https://us-central1-binaryfractal-random-code.cloudfunctions.net/api'; 
+    static PLATFORM_KEY = 'PLATFORM_KEY'; 
+    static LAST_DAY_GENERATED_KEY = 'LAST_DAY_GENERATED_KEY'; 
+    static TOTAL_GENERATED_KEY = 'TOTAL_GENERATED_KEY'; 
+    static URL = 'https://{URL_API}'; 
 }
